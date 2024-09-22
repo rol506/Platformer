fd7c1842ea83114402be2e10763013a35d8a36c6
rol506 <rolromva@gmail.com>
2024-09-22 16:45:40 +0200

    added rectangle with color animation

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,42 @@
 int g_windowSizeX = 640;
 int g_windowSizeY = 360;
 
+unsigned int FPSRestriction = 0; //Set this to 0 will disable fps restriction
+
+GLfloat vertecies[] = {
+    // X      Y     Z     R     G     B
+     -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,      //    1 --- 4
+     -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,      //    | \   |
+      0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,      //    |   \ |
+      0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f       //    2 --- 3
+};
+
+unsigned int elements[] = {
+    0, 1, 2,
+    0, 2, 3
+};
+
+const char* vertexSource =
+"#version 330 core\n;"
+"layout(location=0) in vec3 vertexPosition;"
+"layout(location=1) in vec3 vertexColor;"
+"out vec3 Color;"
+"void main()"
+"{"
+"gl_Position = vec4(vertexPosition, 1.0);"
+"Color = vertexColor;"
+"}";
+
+const char* fragmentSource =
+"#version 330 core\n"
+"in vec3 Color;"
+"out vec4 OutColor;"
+"uniform float timer;"
+"void main()"
+"{"
+"OutColor = vec4(Color.xy, timer, 1.0);"
+"}";
+
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
 {
     g_windowSizeX = width;
@@ -51,23 +87,89 @@ int main(void)
     std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
     std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
 
-
-    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
 
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true); //close when escape pressed
+        GLuint vertex, fragment;
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vertexSource, nullptr);
+        glCompileShader(vertex);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fragmentSource, nullptr);
+        glCompileShader(fragment);
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        GLuint shader = glCreateProgram();
+        glAttachShader(shader, vertex);
+        glAttachShader(shader, fragment);
+        glLinkProgram(shader);
+
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+
+        GLuint VBO, EBO, VAO;
+        glGenBuffers(1, &VBO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertecies), vertecies, GL_STATIC_DRAW);
+
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        float timer;
+
+        //for FPS counter
+        double lastTime = glfwGetTime();
+        int nbFrames = 0;
+
+        glClearColor(0.5f, 1.0f, 1.0f, 1.0f);
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            //FPS counter
+            double currentTime = glfwGetTime();
+            if (currentTime - lastTime >= 1.0)
+            {
+                //print fps and reset timer
+                std::cout << "FPS: " << nbFrames << "\n";
+                nbFrames = 0;
+                lastTime += 1.0f;
+            }
+
+            if (nbFrames < FPSRestriction || FPSRestriction == 0)
+            {
+                nbFrames++;
+
+                /* Render here */
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                timer = sin(glfwGetTime()) / 2.0f + 0.5f;
+                glUniform1f(glGetUniformLocation(shader, "timer"), timer);
+
+                glBindVertexArray(VAO);
+                glUseProgram(shader);
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+            }
+
+            if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true); //close when escape pressed
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
+        return 0;
     }
-
-    glfwTerminate();
-    return 0;
 }
