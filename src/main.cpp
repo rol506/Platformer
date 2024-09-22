8f0106775863578239d3e8cddc0aeff317e0a54b
rol506 <rolromva@gmail.com>
2024-09-22 08:58:36 +0200

    Added glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,26 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cerr << "glfwInit failed!\n";
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
     {
+        std::cerr << "Failed to create window!\n";
         glfwTerminate();
         return -1;
     }
@@ -19,11 +28,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGL())
+    {
+        std::cerr << "Failed to load GLaD!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
+
+
+    glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
